06d435a0fa6c24ee7ec8fea0b6327ded8d2ff8ec
Mustafa Özkan <me@mustafaoezkan.com.tr>
2025-06-28 01:32:08 +0300

    config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/osmanogluendustriyel.com',
-  assetPrefix: '/osmanogluendustriyel/',
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
